d4678be168a9a1d308d1190ddadd0cec2a14b851
Achyut <achyutp10@gmail.com>
2025-09-27 19:41:14 +0545

    web

--- frontend/web-app/app/auctions/CarImage.tsx
+++ frontend/web-app/app/auctions/CarImage.tsx
@@ -1,25 +1,29 @@
-"use client";
-import Image from "next/image";
-import React, { useState } from "react";
+'use client';
+
+import React, { useState } from 'react'
+import Image from 'next/image';
 
 type Props = {
-  imageUrl: string;
-};
-export default function CarImage({ imageUrl }: Props) {
-  const [loading, setLoading] = useState(true);
+    imageUrl: string;
+}
 
-  return (
-    <Image
-      src={imageUrl}
-      alt="Image of car"
-      fill
-      className={`
-        object-cover duration-700 ease-in-out
-        ${loading ? "opacity-0 scale-110" : "opacity-100 scale-100"}
-        `}
-      priority
-      sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw, 25vw"
-      onLoad={() => setLoading(false)}
-    />
-  );
+export default function CarImage({imageUrl}: Props) {
+    const [isLoading, setLoading] = useState(true);
+
+    return (
+        <Image
+            src={imageUrl}
+            fill
+            alt='image of car'
+            priority
+            className={`
+                    object-cover group-hover:opacity-75 duration-700 ease-in-out
+                    ${isLoading 
+                        ? 'grayscale blur-2xl scale-110' 
+                        : 'grayscale-0 blur-0 scale-100'}
+                `}
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw'
+            onLoad={() => setLoading(false)}
+        />
+    )
 }
--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import Search from "./Search";
-import Logo from "./Logo";
+import Search from './Search';
+import Logo from './Logo';
+import LoginButton from './LoginButton';
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export default function NavBar() {
-  return (
-    <header className="sticky top-0 z-50 flex justify-between bg-[#F7F8F9] p-5 items-center text-gray-800 shadow-md">
-      <Logo />
-      <Search />
-      <div>Login</div>
-    </header>
-  );
+export default async function Navbar() {
+    const user = await getCurrentUser();
+    return (
+        <header className='sticky top-0 z-50 flex justify-between bg-white shadow-md py-5 px-5 items-center text-gray-800'>
+            <Logo />
+            <Search />
+            {user ? (
+                <UserActions user={user} />
+            ) : (
+                <LoginButton />
+            )}
+
+        </header>
+    );
 }
